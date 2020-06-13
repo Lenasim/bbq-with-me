de63f4eefaeea6d675cc070ce6f5b93b476e6945
Lenasim <radical.rin@gmail.com>
2020-06-13 23:00:47 +0200

    Set initial app.js with 2 buttons

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button, StyleSheet } from "react-native";
+
+
 
 export default function App() {
-  return (
+return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Button
+            onPress={onPressLearnMore}
+            title="Learn More"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+            onPress={onPressLearnMore}
+            title="Learn More"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
     </View>
-  );
+)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      justifyContent: "center",
+      alignContent: "center",
+      backgroundColor: "#A3CB38",
   },
-});
+  text: {
+      color: "#dff9fb",
+      fontSize: 30,
+      textAlign: "center",
+  }
+})
