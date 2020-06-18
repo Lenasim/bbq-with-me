d0a1a415e7335a7663b18bd38b20e8a4a775f5e6
LenaSim <radical.rin@gmail.com>
2020-06-18 14:21:25 +0200

    Read me added

--- App.js
+++ App.js
@@ -4,34 +4,34 @@ import { View, Button, StyleSheet } from "react-native";
 
 
 export default function App() {
-return (
-    <View style={styles.container}>
-        <Button
-            onPress={onPressLearnMore}
-            title="Learn More"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-            onPress={onPressLearnMore}
-            title="Learn More"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-        />
-    </View>
-)
+    return (
+        <View style={styles.container}>
+            <Button
+                onPress={onPressLearnMore}
+                title="Learn More"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+                onPress={onPressLearnMore}
+                title="Learn More"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      justifyContent: "center",
-      alignContent: "center",
-      backgroundColor: "#A3CB38",
-  },
-  text: {
-      color: "#dff9fb",
-      fontSize: 30,
-      textAlign: "center",
-  }
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+        backgroundColor: "#A3CB38",
+    },
+    text: {
+        color: "#dff9fb",
+        fontSize: 30,
+        textAlign: "center",
+    }
 })
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-#BBQ with me
-From choosing the date with the best weather to cooking perfectly with timer
+# BBQ with me
+From choosing the date with the best weather to cooking perfectly with time
+-NOW IN PROGRESS-
+
+## React native
+
+This repository contains an exercise to learn React native and Expo. 
+The API 'Open weather map' is used to get weather information based on actual location.
